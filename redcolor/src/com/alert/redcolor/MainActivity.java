a2a67ed9eca7a9ddb690064cc3273bbb6eb0a1b2
ranocohen <ranocohen@gmail.com>
2014-07-09 23:26:44 +0300

    map placed within fragment via support lib hack

--- redcolor/src/com/alert/redcolor/MainActivity.java
+++ redcolor/src/com/alert/redcolor/MainActivity.java
@@ -1,152 +1,303 @@
+/*
+ * Copyright 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.alert.redcolor;
 
-import org.joda.time.DateTime;
-
-import com.alert.redcolor.db.AlertProvider;
-import com.alert.redcolor.db.RedColordb;
-import com.alert.redcolor.model.Alert;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.app.ActionBar;
-import android.app.ActionBar.Tab;
-import android.app.ActionBar.TabListener;
-import android.app.Activity;
-import android.app.Fragment;
 import android.app.FragmentTransaction;
-import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity   {
+public class MainActivity extends FragmentActivity implements
+		ActionBar.TabListener, ConnectionCallbacks, OnConnectionFailedListener,
+		LocationListener {
 
+	// Location related variables
+	LocationRequest locationRequest;
+	LocationClient locationClient;
+	boolean locationEnabled;
+	/**
+	 * The {@link android.support.v4.view.PagerAdapter} that will provide
+	 * fragments for each of the three primary sections of the app. We use a
+	 * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
+	 * will keep every loaded fragment in memory. If this becomes too memory
+	 * intensive, it may be best to switch to a
+	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+	 */
+	AppSectionsPagerAdapter mAppSectionsPagerAdapter;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	/**
+	 * The {@link ViewPager} that will display the three primary sections of the
+	 * app, one at a time.
+	 */
+	ViewPager mViewPager;
+
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		   // Notice that setContentView() is not used, because we use the root
-	    // android.R.id.content as the container for each fragment
-
+		setContentView(R.layout.activity_main);
 		
-	
-		//addAlert(new Alert(1,"ASHKELON",2000,1000,new DateTime()));
-		//addAlert(new Alert(2,"BATYAM",3000,1000,new DateTime()));
-		
-		
-		
-		
-		
-		
-		
-	    // setup action bar for tabs
-	    ActionBar actionBar = getActionBar();
-	    actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-	    actionBar.setDisplayShowTitleEnabled(false);
-
-	    Tab tab = actionBar.newTab()
-	                       .setText("Tab1")
-	                       .setTabListener(new TabListener<PlaceholderFragment>(
-	                               this, "tab1", PlaceholderFragment.class));
-	    actionBar.addTab(tab);
-
-	    tab = actionBar.newTab()
-	                   .setText("Tab2")
-	                   .setTabListener(new TabListener<AlertsListFragment>(
-	                           this, "tab2", AlertsListFragment.class));
-	    actionBar.addTab(tab);
-	}
-
-	private void addAlert(Alert alert) {
-		ContentValues cv = new ContentValues();
-		cv.put(RedColordb.Columns.ID , alert.getId());
-		cv.put(RedColordb.Columns.xCord , alert.getX());
-		cv.put(RedColordb.Columns.yCord , alert.getY());
-		cv.put(RedColordb.Columns.location , alert.getLocation());
-		cv.put(RedColordb.Columns.time , alert.getTime().toString());
-		
-		getContentResolver().insert(
-				AlertProvider.ALERTS_CONTENT_URI, cv);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		LocationManager manager = (LocationManager) getApplication().getSystemService(Context.LOCATION_SERVICE);
+		if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER) && !manager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+		    locationEnabled = false;
+		    Toast.makeText(getApplication(), "Enable location services for accurate data", Toast.LENGTH_SHORT).show();
 		}
-		return super.onOptionsItemSelected(item);
+		else locationEnabled = true;
+
+		locationClient = new LocationClient(this, this, this);
+		locationRequest = new LocationRequest();
+		// Use high accuracy
+		locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+		// Set the update interval to 5 seconds
+		locationRequest.setInterval(5000);
+		// Set the fastest update interval to 1 second
+		locationRequest.setFastestInterval(1000);
+
+		// Create the adapter that will return a fragment for each of the three
+		// primary sections
+		// of the app.
+		mAppSectionsPagerAdapter = new AppSectionsPagerAdapter(
+				getSupportFragmentManager());
+
+		// Set up the action bar.
+		final ActionBar actionBar = getActionBar();
+
+		// Specify that the Home/Up button should not be enabled, since there is
+		// no hierarchical
+		// parent.
+		actionBar.setHomeButtonEnabled(false);
+
+		// Specify that we will be displaying tabs in the action bar.
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		// Set up the ViewPager, attaching the adapter and setting up a listener
+		// for when the
+		// user swipes between sections.
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager.setAdapter(mAppSectionsPagerAdapter);
+		mViewPager
+				.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+					@Override
+					public void onPageSelected(int position) {
+						// When swiping between different app sections, select
+						// the corresponding tab.
+						// We can also use ActionBar.Tab#select() to do this if
+						// we have a reference to the
+						// Tab.
+						actionBar.setSelectedNavigationItem(position);
+					}
+				});
+
+		// For each of the sections in the app, add a tab to the action bar.
+		for (int i = 0; i < mAppSectionsPagerAdapter.getCount(); i++) {
+			// Create a tab with text corresponding to the page title defined by
+			// the adapter.
+			// Also specify this Activity object, which implements the
+			// TabListener interface, as the
+			// listener for when this tab is selected.
+			actionBar.addTab(actionBar.newTab()
+					.setText(mAppSectionsPagerAdapter.getPageTitle(i))
+					.setTabListener(this));
+		}
+	}
+
+	@Override
+	public void onTabUnselected(ActionBar.Tab tab,
+			FragmentTransaction fragmentTransaction) {
+	}
+
+	@Override
+	public void onTabSelected(ActionBar.Tab tab,
+			FragmentTransaction fragmentTransaction) {
+		// When the given tab is selected, switch to the corresponding page in
+		// the ViewPager.
+		mViewPager.setCurrentItem(tab.getPosition());
+	}
+
+	@Override
+	public void onTabReselected(ActionBar.Tab tab,
+			FragmentTransaction fragmentTransaction) {
 	}
 
 	/**
-	 * A placeholder fragment containing a simple view.
+	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+	 * one of the primary sections of the app.
 	 */
-	public static class PlaceholderFragment extends Fragment {
+	public static class AppSectionsPagerAdapter extends FragmentPagerAdapter {
 
-		public PlaceholderFragment() {
+		public AppSectionsPagerAdapter(FragmentManager fm) {
+			super(fm);
 		}
+
+		@Override
+		public Fragment getItem(int i) {
+			switch (i) {
+			case 0:
+				// The first section of the app is the most interesting -- it
+				// offers
+				// a launchpad into the other demonstrations in this example
+				// application.
+				// return new LaunchpadSectionFragment();
+				return new CustomMapFragment();
+
+			default:
+				// The other sections of the app are dummy placeholders.
+				Fragment fragment = new DummySectionFragment();
+				Bundle args = new Bundle();
+				args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, i + 1);
+				fragment.setArguments(args);
+				return fragment;
+			}
+		}
+
+		@Override
+		public int getCount() {
+			return 3;
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			return "Section " + (position + 1);
+		}
+	}
+
+	/**
+	 * A fragment that launches other parts of the demo application.
+	 */
+	public static class LaunchpadSectionFragment extends Fragment {
 
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
+			View rootView = inflater.inflate(
+					R.layout.fragment_section_launchpad, container, false);
+
+			// Demonstration of a collection-browsing activity.
+			rootView.findViewById(R.id.demo_collection_button)
+					.setOnClickListener(new View.OnClickListener() {
+						@Override
+						public void onClick(View view) {
+							Intent intent = new Intent(getActivity(),
+									CollectionDemoActivity.class);
+							startActivity(intent);
+						}
+					});
+
+			// Demonstration of navigating to external activities.
+			rootView.findViewById(R.id.demo_external_activity)
+					.setOnClickListener(new View.OnClickListener() {
+						@Override
+						public void onClick(View view) {
+							// Create an intent that asks the user to pick a
+							// photo, but using
+							// FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET, ensures that
+							// relaunching
+							// the application from the device home screen does
+							// not return
+							// to the external activity.
+							Intent externalActivityIntent = new Intent(
+									Intent.ACTION_PICK);
+							externalActivityIntent.setType("image/*");
+							externalActivityIntent
+									.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+							startActivity(externalActivityIntent);
+						}
+					});
+
 			return rootView;
 		}
 	}
-	
-	public static class TabListener<T extends Fragment> implements ActionBar.TabListener {
-	    private Fragment mFragment;
-	    private final Activity mActivity;
-	    private final String mTag;
-	    private final Class<T> mClass;
 
-	    /** Constructor used each time a new tab is created.
-	      * @param activity  The host Activity, used to instantiate the fragment
-	      * @param tag  The identifier tag for the fragment
-	      * @param clz  The fragment's Class, used to instantiate the fragment
-	      */
-	    public TabListener(Activity activity, String tag, Class<T> clz) {
-	        mActivity = activity;
-	        mTag = tag;
-	        mClass = clz;
-	    }
+	/**
+	 * A dummy fragment representing a section of the app, but that simply
+	 * displays dummy text.
+	 */
+	public static class DummySectionFragment extends Fragment {
 
-	    /* The following are each of the ActionBar.TabListener callbacks */
+		public static final String ARG_SECTION_NUMBER = "section_number";
 
-	    public void onTabSelected(Tab tab, FragmentTransaction ft) {
-	        // Check if the fragment is already initialized
-	        if (mFragment == null) {
-	            // If not, instantiate and add it to the activity
-	            mFragment = Fragment.instantiate(mActivity, mClass.getName());
-	            ft.add(android.R.id.content, mFragment, mTag);
-	        } else {
-	            // If it exists, simply attach it in order to show it
-	            ft.attach(mFragment);
-	        }
-	    }
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_section_dummy,
+					container, false);
+			Bundle args = getArguments();
+			((TextView) rootView.findViewById(android.R.id.text1))
+					.setText(getString(R.string.dummy_section_text,
+							args.getInt(ARG_SECTION_NUMBER)));
+			return rootView;
+		}
+	}
 
-	    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-	        if (mFragment != null) {
-	            // Detach the fragment, because another one is being attached
-	            ft.detach(mFragment);
-	        }
-	    }
+	@Override
+	public void onLocationChanged(Location location) {
+		locationClient.removeLocationUpdates(this);
 
-	    public void onTabReselected(Tab tab, FragmentTransaction ft) {
-	        // User selected the already selected tab. Usually do nothing.
-	    }
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onConnected(Bundle connectionHint) {
+		Location location = locationClient.getLastLocation();
+		if (location == null)
+			locationClient.requestLocationUpdates(locationRequest, this);
+		
+		if (location != null) {
+		    Toast.makeText(getApplication(), "Location: " + location.getLatitude() + ", " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+		}
+		else if (location == null && locationEnabled) {
+		    locationClient.requestLocationUpdates(locationRequest, this);
+		}
+		// else
+		/*
+		 * Toast.makeText( getActivity(), "Location: " + location.getLatitude()
+		 * + ", " + location.getLongitude(), Toast.LENGTH_SHORT) .show();
+		 */
+
+	}
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+
 	}
 }
---
+++ redcolor/src/com/alert/redcolor/MainActivityOld.java
@@ -0,0 +1,152 @@
+package com.alert.redcolor;
+
+import org.joda.time.DateTime;
+
+import com.alert.redcolor.db.AlertProvider;
+import com.alert.redcolor.db.RedColordb;
+import com.alert.redcolor.model.Alert;
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.ContentValues;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivityOld extends Activity   {
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		   // Notice that setContentView() is not used, because we use the root
+	    // android.R.id.content as the container for each fragment
+
+		
+	
+		//addAlert(new Alert(1,"ASHKELON",2000,1000,new DateTime()));
+		//addAlert(new Alert(2,"BATYAM",3000,1000,new DateTime()));
+		
+		
+		
+		
+		
+		
+		
+	    // setup action bar for tabs
+	    ActionBar actionBar = getActionBar();
+	    actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+	    actionBar.setDisplayShowTitleEnabled(false);
+
+	    Tab tab = actionBar.newTab()
+	                       .setText("Tab1")
+	                       .setTabListener(new TabListener<PlaceholderFragment>(
+	                               this, "tab1", PlaceholderFragment.class));
+	    actionBar.addTab(tab);
+
+	    tab = actionBar.newTab()
+	                   .setText("Tab2")
+	                   .setTabListener(new TabListener<AlertsListFragment>(
+	                           this, "tab2", AlertsListFragment.class));
+	    actionBar.addTab(tab);
+	}
+
+	private void addAlert(Alert alert) {
+		ContentValues cv = new ContentValues();
+		cv.put(RedColordb.Columns.ID , alert.getId());
+		cv.put(RedColordb.Columns.xCord , alert.getX());
+		cv.put(RedColordb.Columns.yCord , alert.getY());
+		cv.put(RedColordb.Columns.location , alert.getLocation());
+		cv.put(RedColordb.Columns.time , alert.getTime().toString());
+		
+		getContentResolver().insert(
+				AlertProvider.ALERTS_CONTENT_URI, cv);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
+	
+	public static class TabListener<T extends Fragment> implements ActionBar.TabListener {
+	    private Fragment mFragment;
+	    private final Activity mActivity;
+	    private final String mTag;
+	    private final Class<T> mClass;
+
+	    /** Constructor used each time a new tab is created.
+	      * @param activity  The host Activity, used to instantiate the fragment
+	      * @param tag  The identifier tag for the fragment
+	      * @param clz  The fragment's Class, used to instantiate the fragment
+	      */
+	    public TabListener(Activity activity, String tag, Class<T> clz) {
+	        mActivity = activity;
+	        mTag = tag;
+	        mClass = clz;
+	    }
+
+	    /* The following are each of the ActionBar.TabListener callbacks */
+
+	    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+	        // Check if the fragment is already initialized
+	        if (mFragment == null) {
+	            // If not, instantiate and add it to the activity
+	            mFragment = Fragment.instantiate(mActivity, mClass.getName());
+	            ft.add(android.R.id.content, mFragment, mTag);
+	        } else {
+	            // If it exists, simply attach it in order to show it
+	            ft.attach(mFragment);
+	        }
+	    }
+
+	    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+	        if (mFragment != null) {
+	            // Detach the fragment, because another one is being attached
+	            ft.detach(mFragment);
+	        }
+	    }
+
+	    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+	        // User selected the already selected tab. Usually do nothing.
+	    }
+	}
+}
