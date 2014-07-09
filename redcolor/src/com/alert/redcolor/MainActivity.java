6ed674140824f7d00011279e7e28abe49b208dc9
Idan <idanakav@gmail.com>
2014-07-09 18:38:09 +0300

    added tabs

--- redcolor/src/com/alert/redcolor/MainActivity.java
+++ redcolor/src/com/alert/redcolor/MainActivity.java
@@ -1,24 +1,44 @@
 package com.alert.redcolor;
 
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity   {
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		   // Notice that setContentView() is not used, because we use the root
+	    // android.R.id.content as the container for each fragment
+
+	    // setup action bar for tabs
+	    ActionBar actionBar = getActionBar();
+	    actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+	    actionBar.setDisplayShowTitleEnabled(false);
+
+	    Tab tab = actionBar.newTab()
+	                       .setText("Tab1")
+	                       .setTabListener(new TabListener<PlaceholderFragment>(
+	                               this, "tab1", PlaceholderFragment.class));
+	    actionBar.addTab(tab);
+
+	    tab = actionBar.newTab()
+	                   .setText("Tab2")
+	                   .setTabListener(new TabListener<PlaceholderFragment>(
+	                           this, "tab2", PlaceholderFragment.class));
+	    actionBar.addTab(tab);
 	}
 
 	@Override
@@ -55,5 +75,48 @@ public class MainActivity extends Activity {
 					false);
 			return rootView;
 		}
+	}
+	
+	public static class TabListener<T extends Fragment> implements ActionBar.TabListener {
+	    private Fragment mFragment;
+	    private final Activity mActivity;
+	    private final String mTag;
+	    private final Class<T> mClass;
+
+	    /** Constructor used each time a new tab is created.
+	      * @param activity  The host Activity, used to instantiate the fragment
+	      * @param tag  The identifier tag for the fragment
+	      * @param clz  The fragment's Class, used to instantiate the fragment
+	      */
+	    public TabListener(Activity activity, String tag, Class<T> clz) {
+	        mActivity = activity;
+	        mTag = tag;
+	        mClass = clz;
+	    }
+
+	    /* The following are each of the ActionBar.TabListener callbacks */
+
+	    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+	        // Check if the fragment is already initialized
+	        if (mFragment == null) {
+	            // If not, instantiate and add it to the activity
+	            mFragment = Fragment.instantiate(mActivity, mClass.getName());
+	            ft.add(android.R.id.content, mFragment, mTag);
+	        } else {
+	            // If it exists, simply attach it in order to show it
+	            ft.attach(mFragment);
+	        }
+	    }
+
+	    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+	        if (mFragment != null) {
+	            // Detach the fragment, because another one is being attached
+	            ft.detach(mFragment);
+	        }
+	    }
+
+	    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+	        // User selected the already selected tab. Usually do nothing.
+	    }
 	}
 }
