56dc2f555ceaa837cf6852dfc055591abc70314e
Idan <idanakav@gmail.com>
2014-07-14 16:47:34 +0300

    fixed merge

--- redcolor/src/com/alert/redcolor/PreferencesUtils.java
+++ redcolor/src/com/alert/redcolor/PreferencesUtils.java
@@ -20,15 +20,5 @@ public class PreferencesUtils {
 	}
 
 	     
-	    
-	    public static final String ALERTS_TYPE_KEY = "pref_alertsType";
-	    public static final String ALERTS_TOWNS_SELECT = "pref_towns_select";
-	    public static final String PREF_ALL_ALERTS = "all";
-	    public static final String PREF_LOCAL_ALERTS = "local";
-	    public static String getAlertsType(Context con) {
-	    	SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(con);
-	    	String alertsType = sharedPref.getString(ALERTS_TYPE_KEY, "local");
-	    	return alertsType;
-	    	
-	    }
+
 }
