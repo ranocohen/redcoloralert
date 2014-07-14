5fbeb01261fb0425410ea2e22ec603650387bb91
ranocohen <ranocohen@gmail.com>
2014-07-14 16:11:31 +0300

    ringtone type selectore added

--- redcolor/src/com/alert/redcolor/PreferencesUtils.java
+++ redcolor/src/com/alert/redcolor/PreferencesUtils.java
@@ -2,20 +2,31 @@ package com.alert.redcolor;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.preference.PreferenceManager;
-
 
 public class PreferencesUtils {
 
-	     
-	    
-	    public static final String ALERTS_TYPE_KEY = "pref_alertsType";
-	    public static final String PREF_ALL_ALERTS = "all";
-	    public static final String PREF_LOCAL_ALERTS = "local";
-	    public static String getAlertsType(Context con) {
-	    	SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(con);
-	    	String alertsType = sharedPref.getString(ALERTS_TYPE_KEY, "local");
-	    	return alertsType;
-	    	
-	    }
+	public static final String ALERTS_TYPE_KEY = "pref_alertsType";
+	public static final String PREF_ALL_ALERTS = "all";
+	public static final String PREF_LOCAL_ALERTS = "local";
+
+	public static String getAlertsType(Context con) {
+		SharedPreferences sharedPref = PreferenceManager
+				.getDefaultSharedPreferences(con);
+		String alertsType = sharedPref.getString(ALERTS_TYPE_KEY, "local");
+		return alertsType;
+
+	}
+
+	public static Uri getRingtone(Context con) {
+		String ringtonePreference;
+		// Get the xml/preferences.xml preferences
+		SharedPreferences sharedPref = PreferenceManager
+				.getDefaultSharedPreferences(con);
+		ringtonePreference = sharedPref.getString("ringtonePref",
+				"DEFAULT_RINGTONE_URI");
+		Uri uri = Uri.parse(ringtonePreference);
+		return uri;
+	}
 }
