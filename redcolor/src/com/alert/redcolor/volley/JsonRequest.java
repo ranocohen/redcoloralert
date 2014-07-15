60cf9cbc7b15274100c3dccce59757a1f7a21ac2
ranocohen <ranocohen@gmail.com>
2014-07-15 17:58:08 +0300

    Json analyzer added

--- redcolor/src/com/alert/redcolor/volley/JsonRequest.java
+++ redcolor/src/com/alert/redcolor/volley/JsonRequest.java
@@ -1,6 +1,7 @@
 package com.alert.redcolor.volley;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.alert.redcolor.analytics.AnalyticsApp;
@@ -50,6 +51,33 @@ public class JsonRequest {
 		// Adding request to request queue
 		AnalyticsApp.getInstance().addToRequestQueue(req, tag_json_arry);
 	}
+	
+	public void analyzeAlertJson(JSONObject response) {
+		try {
+			JSONArray data = response.getJSONArray("data");
+			//iterates on each alert
+			for (int i = 0; i < data.length(); i++) {
+				JSONObject alert = data.getJSONObject(i);
+				JSONArray areas = alert.getJSONArray("areas");
+				//area's shit:
+				for (int j = 0; j < areas.length(); j++) {
+					JSONObject area = areas.getJSONObject(j);
+					String area_name = area.getString("area_name"); //TODO IDAN DB
+					JSONArray locations = area.getJSONArray("locations");
+					for (int k = 0; k < locations.length(); k++) {
+						JSONObject location = locations.getJSONObject(k);
+						String location_name = location.getString("name_he");
+						double lat = location.getDouble("lat");
+						double lng = location.getDouble("lng");
+						//TODO WHATEVER YOU WANT TO (DB)
+					}
+				}
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 	public void requestJsonObject(String url, Context con) {
 		// Tag used to cancel the request
@@ -65,6 +93,7 @@ public class JsonRequest {
 					@Override
 					public void onResponse(JSONObject response) {
 						Log.d("VolleyJsonObjectOnResponse", response.toString());
+						analyzeAlertJson(response);
 						pDialog.hide();
 					}
 				}, new Response.ErrorListener() {
