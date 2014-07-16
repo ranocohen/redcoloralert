2f8caa42dad10dc60fab28517eff515bd92886d3
ranocohen <ranocohen@gmail.com>
2014-07-16 10:52:02 +0300

    ran

--- redcolor/src/com/alert/redcolor/SettingsActivity.java
+++ redcolor/src/com/alert/redcolor/SettingsActivity.java
@@ -32,7 +32,6 @@ public class SettingsActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		con = getApplicationContext();
 		// Display the fragment as the main content.
 		getFragmentManager().beginTransaction()
 				.replace(android.R.id.content, new SettingsFragment()).commit();
