cfb592f6d41d42be7a0fbdd7005df1db19449d1e
Idan <idanakav@gmail.com>
2014-07-18 14:52:48 +0300

    moved share image to fragment

--- redcolor/src/com/alert/redcolor/GoogleMapFragment.java
+++ redcolor/src/com/alert/redcolor/GoogleMapFragment.java
@@ -1,5 +1,11 @@
 package com.alert.redcolor;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 import org.joda.time.DateTime;
 import org.joda.time.Minutes;
 import org.joda.time.Seconds;
@@ -7,15 +13,27 @@ import org.joda.time.Seconds;
 import android.app.Activity;
 import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.TextView;
 
 import com.alert.redcolor.analytics.AnalyticsApp;
 import com.alert.redcolor.analytics.AnalyticsApp.TrackerName;
@@ -27,6 +45,7 @@ import com.alert.redcolor.model.City;
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.SnapshotReadyCallback;
 import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -156,4 +175,192 @@ public class GoogleMapFragment extends SupportMapFragment implements LoaderCallb
 	public void onLoaderReset(Loader<Cursor> loader) {
 
 	}
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	
+		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
+	}
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		super.onCreateOptionsMenu(menu, inflater);
+		inflater.inflate(R.menu.map_menu, menu);
+		
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+				switch (item.getItemId()) {
+				
+				case R.id.share:
+					// dick shit fuck face thing
+					shareScreenShotTask screenTask = new shareScreenShotTask();
+					screenTask.execute();
+					return true;
+				default:
+					return super.onOptionsItemSelected(item);
+				}
+		
+	}
+	 class shareScreenShotTask extends AsyncTask<Void, Void, Void>    {
+    	 
+	        TextView tv;
+	 
+	        shareScreenShotTask()    {
+	                     
+	        }
+	 
+	        // Executed on the UI thread before the
+	        // time taking task begins
+	        @Override
+	        protected void onPreExecute() {
+	            super.onPreExecute();
+	        }
+	 
+	        // Executed on a special thread and all your
+	        // time taking tasks should be inside this method
+	        @Override
+	        protected Void doInBackground(Void... arg0) {
+	        	captureMapScreen();
+	            return null;
+	        }
+	        
+	        // Executed on the UI thread after the
+	        // time taking process is completed
+	        @Override
+	        protected void onPostExecute(Void result) {
+	            super.onPostExecute(result);
+	        }
+	    }   
+		
+		public void captureMapScreen() {
+	        SnapshotReadyCallback callback = new SnapshotReadyCallback() {
+
+	            @Override
+	            public void onSnapshotReady(Bitmap snapshot) {
+	                try {
+	                	//check status bar height 
+	                	Rect rectangle= new Rect();
+	                	Window window= getActivity().getWindow();
+	                	window.getDecorView().getWindowVisibleDisplayFrame(rectangle);
+	                	int statusBarHeight= rectangle.top;
+	                	int contentViewTop= 
+	                	    window.findViewById(Window.ID_ANDROID_CONTENT).getTop();
+	                	int titleBarHeight= contentViewTop - statusBarHeight;
+
+	                	   Log.i("*** Jorgesys :: ", "StatusBar Height= " + statusBarHeight + " , TitleBar Height = " + titleBarHeight);
+	                	   
+	                	// Calculate ActionBar height
+	                	   
+	                	   int actionBarHeight = 0;
+	/*                	   TypedValue tv = new TypedValue();
+	                	   if (getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true))
+	                	   {
+	                		   actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data,getResources().getDisplayMetrics());
+	                	   }*/
+	                	   actionBarHeight = getActivity().getActionBar().getHeight();
+	                	//end checking
+	                	
+	            		View mView = getActivity().findViewById(android.R.id.content).getRootView();
+	                    mView.setDrawingCacheEnabled(true);
+	                    Bitmap backBitmap = mView.getDrawingCache();
+	                    Bitmap bmOverlay = Bitmap.createBitmap(
+	                            backBitmap.getWidth(), backBitmap.getHeight(),
+	                            backBitmap.getConfig());
+	                    Canvas canvas = new Canvas(bmOverlay);
+	                    canvas.drawBitmap(backBitmap, 0, 0, null);
+	                    canvas.drawBitmap(snapshot, 0,statusBarHeight+actionBarHeight, null);
+	                    
+	                    Bitmap bm = Bitmap.createBitmap(
+	                            backBitmap.getWidth(), backBitmap.getHeight()-statusBarHeight,
+	                            backBitmap.getConfig());
+	                    
+	                    bm = Bitmap.createBitmap(bmOverlay, 0, statusBarHeight, backBitmap.getWidth(), backBitmap.getHeight()-statusBarHeight);
+	                    
+	                    
+	                    String path = Environment.getExternalStorageDirectory()
+	                            + "/MapScreenShot"
+	                            + System.currentTimeMillis() + ".png";
+	                    File file = new File(path);
+	                    FileOutputStream out = new FileOutputStream(file);
+
+	                    //bmOverlay.compress(Bitmap.CompressFormat.PNG, 90, out);
+	                    bm.compress(Bitmap.CompressFormat.PNG, 90, out);
+	                    
+	            		try {
+	            			out.flush();
+	            			out.close();
+	            		} catch (IOException e1) {
+	            			// TODO Auto-generated catch block
+	            			e1.printStackTrace();
+	            		}
+	            		
+	            		Intent share = new Intent(Intent.ACTION_SEND);
+
+	            		// If you want to share a png image only, you can do:
+
+	            		// setType("image/png"); OR for jpeg: setType("image/jpeg");
+	            		share.setType("image/*");
+
+	            		// Make sure you put example png image named myImage.png in your
+	            		// directory
+
+	            		Uri uri = Uri.fromFile(file);
+	            		share.putExtra(Intent.EXTRA_STREAM, uri);
+
+	            		startActivity(Intent.createChooser(share, "Share Image!"));
+	                    
+	                    
+	                } catch (Exception e) {
+	                    e.printStackTrace();
+	                }
+	            }
+	        };
+
+	        if(getMap()!=null)
+	        	getMap().snapshot(callback);
+
+	    }
+
+		private void shareImage() {
+			Intent share = new Intent(Intent.ACTION_SEND);
+
+			View screen = getActivity().getWindow().getDecorView().findViewById(
+					android.R.id.content);
+			screen.setDrawingCacheEnabled(true);
+			Bitmap bm = screen.getDrawingCache();
+			
+			String path = Environment.getExternalStorageDirectory().toString();
+			OutputStream fOut = null;
+			File file = new File(path, "screenshot.jpg");
+			try {
+				fOut = new FileOutputStream(file);
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+			bm.compress(Bitmap.CompressFormat.JPEG, 85, fOut);
+			try {
+				fOut.flush();
+				fOut.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+
+			// If you want to share a png image only, you can do:
+
+			// setType("image/png"); OR for jpeg: setType("image/jpeg");
+			share.setType("image/*");
+
+			// Make sure you put example png image named myImage.png in your
+			// directory
+
+			Uri uri = Uri.fromFile(file);
+			share.putExtra(Intent.EXTRA_STREAM, uri);
+
+			startActivity(Intent.createChooser(share, "Share Image!"));
+		}
 }
